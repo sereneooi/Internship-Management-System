15ccd11d775a3559b26c7910b5d4f138d2453082
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-20 13:41:46 +0800

    Add files via upload

---
+++ CloudComputing/user_registration/header.php
@@ -0,0 +1,13 @@
+<html>
+<head> 
+    <link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/jquery-ui.css" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/jquery.dataTables.css" />
+    </head>
+    <nav class = "navbar">
+	<div class = "container-fluid">
+		<img class = "pull-left" src="../images/logo.png" width="40%" height="120" alt="logo"  style ="display: block; margin-left: 30%;margin-top: 40px;">
+	<div>
+</nav>
+</html>
