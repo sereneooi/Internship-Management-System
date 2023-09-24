5fa6b7684efbfc5113cabe1c293e1f658defbefd
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-24 14:00:06 +0800

    Update admin_report.php

--- CloudComputing/admin/admin_report.php
+++ CloudComputing/admin/admin_report.php
@@ -5,20 +5,32 @@ require_once '../connect.php';
 $query = $conn->query("SELECT * FROM `admin`") or die($conn->error);
 $admins = $query->fetch_all(MYSQLI_ASSOC);
 
-// Generate the HTML report
+// Generate the HTML report with improved formatting
 $report = '<html>';
-$report .= '<head><title>Admin Report</title></head>';
+$report .= '<head>';
+$report .= '<title>Admin Report</title>';
+$report .= '<style>';
+$report .= 'body { font-family: Arial, sans-serif; margin: 20px; }';
+$report .= 'h1 { text-align: center; }';
+$report .= 'table { width: 100%; border-collapse: collapse; }';
+$report .= 'th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }';
+$report .= 'tr:nth-child(even) { background-color: #f2f2f2; }';
+$report .= 'th { background-color: #4CAF50; color: white; }';
+$report .= '</style>';
+$report .= '</head>';
 $report .= '<body>';
 $report .= '<h1>Admin Report</h1>';
 $report .= '<table>';
 $report .= '<thead><tr><th>Username</th><th>Name</th></tr></thead>';
 $report .= '<tbody>';
+
 foreach ($admins as $admin) {
     $report .= '<tr>';
     $report .= '<td>' . $admin['username'] . '</td>';
     $report .= '<td>' . $admin['name'] . '</td>';
     $report .= '</tr>';
 }
+
 $report .= '</tbody>';
 $report .= '</table>';
 $report .= '</body>';
@@ -30,3 +42,4 @@ header('Content-Type: text/html'); // Change to the appropriate content type
 // Output the generated report
 echo $report;
 ?>
+
