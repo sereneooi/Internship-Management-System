b5907c6d86ac74e42209acbf8e6e468854b2bdf5
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-24 12:44:36 +0800

    Update dbconfig.php

--- CloudComputing/file_uploading/dbconfig.php
+++ CloudComputing/file_uploading/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "db_issm";
+$dbhost = "db1.cpg9cjdwwtu2.us-east-1.rds.amazonaws.com";
+$dbuser = "main";
+$dbpass = "qwer1234";
+$dbname = "db1";
 $conn= mysqli_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
 mysqli_select_db($conn,$dbname) or die('database selection problem');
 ?>
