a937c6cb98520ad65752c36119f314f95bffb4f6
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-24 12:04:33 +0800

    Add files via upload

--- CloudComputing/user_registration/header.php
+++ CloudComputing/user_registration/header.php
@@ -4,10 +4,11 @@
 		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
 		<link rel = "stylesheet" type = "text/css" href = "../css/jquery-ui.css" />
 		<link rel = "stylesheet" type = "text/css" href = "../css/jquery.dataTables.css" />
-    </head>
-    <nav class = "navbar">
+</head>
+
+    <nav class = "navbar" style='margin-bottom:-50px;'>
 	<div class = "container-fluid">
-		<img class = "pull-left" src="../images/logo.png" width="40%" height="120" alt="logo"  style ="display: block; margin-left: 30%;margin-top: 40px;">
+		<img class = "pull-left" src="../images/logo.png" width="40%" height="120" alt="logo" style ="display: block; margin-left: 30%;margin-top: 40px;">
 	<div>
-</nav>
+	</nav>
 </html>
--- CloudComputing/user_registration/index.php
+++ CloudComputing/user_registration/index.php
@@ -59,100 +59,134 @@ if(!empty($_POST["register-user"])) {
 <head>
 <title>PHP User Registration Form</title>
 <style>
-body{
-	width:100%;
-	font-family:calibri;
-}
-.error-message {
-	padding: 7px 10px;
-	background: #fff1f2;
-	border: #ffd5da 1px solid;
-	color: #d6001c;
-	border-radius: 4px;
-}
-.success-message {
-	padding: 7px 10px;
-	background: #cae0c4;
-	border: #c3d0b5 1px solid;
-	color: #027506;
-	border-radius: 4px;
-}
-.demo-table {
-	background:white;
-	width: 100%;
-	border-spacing: initial;
-	margin: 2px 0px;
-	word-break: break-word;
-	table-layout: auto;
-	line-height: 1.8em;
-	color: #333;
-	border-radius: 4px;
-	padding: 20px 40px;
-    filter: alpha(opacity=80); /* For IE8 and earlier */
-    text-align: center;
-}
-.demo-table td {
-	padding: 15px 0px;
-}
-.demoInputBox {
-	padding: 10px 30px;
-	border: #a9a9a9 1px solid;
-	border-radius: 4px;
-}
-.btnRegister {
-	padding: 10px 30px;
-	background-color: #3367b2;
-	border: 0;
-	color: #FFF;
-	cursor: pointer;
-	border-radius: 4px;
-	margin-left: 10px;
-}
-</style>
+        body {
+            width: 100%;
+            font-family: calibri;
+			background-image: url('../images/blur2.jpg');
+			background-repeat: no-repeat;
+			background-size: cover;
+        }
+
+        .container {
+            width: 30%;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: white;
+            border-radius: 4px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+        }
+
+        .error-message {
+            padding: 7px 10px;
+            background: #fff1f2;
+            border: #ffd5da 1px solid;
+            color: #d6001c;
+            border-radius: 4px;
+            margin-bottom: 10px;
+            text-align: center;
+        }
+
+        .success-message {
+            padding: 7px 10px;
+            background: #cae0c4;
+            border: #c3d0b5 1px solid;
+            color: #027506;
+            border-radius: 4px;
+            margin-bottom: 10px;
+            text-align: center;
+        }
+
+        .demo-table {
+            width: 100%;
+            border-spacing: initial;
+            margin: 2px 0px;
+            word-break: break-word;
+            table-layout: auto;
+            line-height: 1.8em;
+            color: #333;
+            border-radius: 4px;
+            padding: 20px 40px;
+            text-align: center;
+        }
+
+        .demo-table td {
+            padding: 15px 0px;
+        }
+
+        .demoInputBox {
+            padding: 10px 30px;
+            border: #a9a9a9 1px solid;
+            border-radius: 4px;
+        }
+
+        .btnRegister {
+            padding: 10px 30px;
+            background-color: #3367b2;
+            border: 0;
+            color: #FFF;
+            cursor: pointer;
+            border-radius: 4px;
+            margin-left: 10px;
+        }
+    </style>
 </head>
 <body>
-<form name="frmRegistration" method="post" action="">
-<table border="0" style='width:30%; margin-left:auto;margin-right:auto;' class="demo-table">
-<?php if(!empty($success_message)) { ?>	
-<div class="success-message" style='text-align:center;'><?php if(isset($success_message)) echo $success_message; ?></div>
-<?php } ?>
-<?php if(!empty($error_message)) { ?>	
-<div class="error-message" style='text-align:center;'><?php if(isset($error_message)) echo $error_message; ?></div>
-<?php } ?>
-<tr>
-<td>User Name</td>
-<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
-</tr>
-<tr>
-<td>First Name</td>
-<td><input type="text" class="demoInputBox" name="firstName" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
-</tr>
-<tr>
-<td>Last Name</td>
-<td><input type="text" class="demoInputBox" name="lastName" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" class="demoInputBox" name="password" value=""></td>
-</tr>
-<tr>
-<td>Confirm Password</td>
-<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-</tr>
-<tr>
-<td>Email</td>
-<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
-</tr>
-<tr>
-<td>Gender</td>
-<td><input type="radio" name="gender" value="Male" <?php if(isset($_POST['gender']) && $_POST['gender']=="Male") { ?>checked<?php  } ?>> Male
-<input type="radio" name="gender" value="Female" <?php if(isset($_POST['gender']) && $_POST['gender']=="Female") { ?>checked<?php  } ?>> Female
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
-</tr>
-</table>
-</form>
+    <div class="container">
+        <form name="frmRegistration" method="post" action="">
+            <table border="0" class="demo-table" style='background-color: white;'>
+                <?php if (!empty($success_message)) { ?>
+                    <tr>
+                        <td colspan="2" class="success-message"><?php if (isset($success_message)) echo $success_message; ?></td>
+                    </tr>
+                <?php } ?>
+                <?php if (!empty($error_message)) { ?>
+                    <tr>
+                        <td colspan="2" class="error-message"><?php if (isset($error_message)) echo $error_message; ?></td>
+                    </tr>
+                <?php } ?>
+                <tr>
+                    <td>User Name</td>
+                    <td><input type="text" class="demoInputBox" name="userName"
+                            value="<?php if (isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>First Name</td>
+                    <td><input type="text" class="demoInputBox" name="firstName"
+                            value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><input type="text" class="demoInputBox" name="lastName"
+                            value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" class="demoInputBox" name="password" value=""></td>
+                </tr>
+                <tr>
+                    <td>Confirm Password</td>
+                    <td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td><input type="text" class="demoInputBox" name="userEmail"
+                            value="<?php if (isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>
+                        <input type="radio" name="gender" value="Male"
+                            <?php if (isset($_POST['gender']) && $_POST['gender'] == "Male") { ?>checked<?php } ?>> Male
+                        <input type="radio" name="gender" value="Female"
+                            <?php if (isset($_POST['gender']) && $_POST['gender'] == "Female") { ?>checked<?php } ?>>
+                        Female
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan=2><input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit"
+                            name="register-user" value="Register" class="btnRegister"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
 </body></html>
