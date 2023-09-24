81f76b76f1fac78a01f620a609b993eb09c07c51
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-24 14:14:45 +0800

    Update aboutus.php

--- CloudComputing/aboutus.php
+++ CloudComputing/aboutus.php
@@ -1,19 +1,32 @@
-<style>
+<div id="aboutus" class="tab-pane fade">
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>VISION</h3></center></div>
+    <h4>Excellence in producing highly skilled, well-qualified, and globally competitive Information Technology Education (ITE) graduates</h4>
+    <br />
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>MISSION</h3></center></div>
+    <h4>Committed to providing affordable and high-quality education for the training and development of ITE professionals equipped with the technological knowledge and skills responsive to the demands of both local and international communities</h4>
+    <br />
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>OBJECTIVE</h3></center></div>
+    <ol>
+        <li><h4>Possess knowledge and skills that would prepare graduates in computing professions who are well-versed in the application, installation, operation, development, maintenance, and administration of IT systems</h4></li>
+        <li><h4>Prepare students to be IT professionals and experts in the design and implementation of Information Systems for business processes</h4></li>
+        <li><h4>Be proficient in designing and developing computing solutions</h4></li>
+        <li><h4>Manifest interpersonal, communications, and leadership skills through significant curricular, co-curricular, and extra-curricular activities</h4></li>
+        <li><h4>Virtue social consciousness through active participation in research, extension, and professional organizations</h4></li>
+    </ol>
+    
+    <!-- Founder 1: Bee Wei Win's Portfolio Section -->
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>Bee Wei Win's Portfolio</h3></center></div>
+    <p>Bachelor's Degree of Computer Science</p>
 
-</style>
-<div id = "aboutus" class = "tab-pane fade">
-	<div class = "alert" style='background-color: #ADD8E6;color:black;'><center><h3>VISION</h3></center></div>
-	<h4>Excellence in producing highly skilled, well qualified and globally competitive Information Technology Education(ITE) graduation</h4>
-	<br />
-	<div class = "alert" style='background-color: #ADD8E6;color:black;'><center><h3>MISSION</h3></center></div>
-	<h4 >Committed to provide affordable and high quality education for the training and development of ITE professionals equipped with the technological knowledge and skills responsive to the demands of both local and international communities</h4>
-	<br />
-	<div class = "alert" style='background-color: #ADD8E6;color:black;'><center><h3>OBJECTIVE</h3></center></div>
-	<ol>
-		<li><h4>Possess knowledge and skills that would prepare graduate in computing professions who are well versed on application, installation, operation, development, maintenance and administration of IT systems</h4></li>
-		<li><h4>Prepare student to be IT professionals and be expert on design and implementation of Information Systems for business processes</h4></li>
-		<li><h4>Be proficient in designing and developing computing solutions</h4></li>
-		<li><h4>Manifest interpersonal, communications, and leadership skills through significant curricular, co-curricular and extra-curricular activities</h4></li>
-		<li><h4>Virtues social consciousness through active participation in research, extension and professional organization</h4></li>
-	</ol>
+    <!-- Founder 2: Foo Jing Lynn's Portfolio Section -->
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>Foo Jing Lynn's Portfolio</h3></center></div>
+    <p>Bachelor's Degree of Computer Science</p>
+
+    <!-- Founder 3: Ho Kai Ying's Portfolio Section -->
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>Ho Kai Ying's Portfolio</h3></center></div>
+    <p>Bachelor's Degree of Computer Science</p>
+
+    <!-- Founder 4: Serene Ooi Yin Ting's Portfolio Section -->
+    <div class="alert" style='background-color: #ADD8E6;color:black;'><center><h3>Serene Ooi Yin Ting's Portfolio</h3></center></div>
+    <p>Bachelor's Degree of Computer Science</p>
 </div>
