2e6c64fa15caaf646d5181a45dd2d22aa06587aa
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-24 14:17:14 +0800

    Update applied_report.php

--- CloudComputing/admin/applied_report.php
+++ CloudComputing/admin/applied_report.php
@@ -2,13 +2,13 @@
 require_once '../connect.php';
 
 // Perform necessary database queries to retrieve data for the report
-$query = $conn->query("SELECT * FROM `admin`") or die($conn->error);
-$admins = $query->fetch_all(MYSQLI_ASSOC);
+$query = $conn->query("SELECT * FROM `fill_details`") or die($conn->error);
+$fillDetails = $query->fetch_all(MYSQLI_ASSOC);
 
 // Generate the HTML report with improved formatting
 $report = '<html>';
 $report .= '<head>';
-$report .= '<title>Admin Report</title>';
+$report .= '<title>Fill Details Report</title>';
 $report .= '<style>';
 $report .= 'body { font-family: Arial, sans-serif; margin: 20px; }';
 $report .= 'h1 { text-align: center; }';
@@ -19,15 +19,19 @@ $report .= 'th { background-color: #4CAF50; color: white; }';
 $report .= '</style>';
 $report .= '</head>';
 $report .= '<body>';
-$report .= '<h1>Admin Report</h1>';
+$report .= '<h1>Fill Details Report</h1>';
 $report .= '<table>';
-$report .= '<thead><tr><th>Username</th><th>Name</th></tr></thead>';
+$report .= '<thead><tr><th>Company Name</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Gender</th><th>CV</th></tr></thead>';
 $report .= '<tbody>';
 
-foreach ($admins as $admin) {
+foreach ($fillDetails as $detail) {
     $report .= '<tr>';
-    $report .= '<td>' . $admin['username'] . '</td>';
-    $report .= '<td>' . $admin['name'] . '</td>';
+    $report .= '<td>' . $detail['company_name'] . '</td>';
+    $report .= '<td>' . $detail['first_name'] . '</td>';
+    $report .= '<td>' . $detail['last_name'] . '</td>';
+    $report .= '<td>' . $detail['email'] . '</td>';
+    $report .= '<td>' . $detail['gender'] . '</td>';
+    $report .= '<td>' . $detail['file'] . '</td>';
     $report .= '</tr>';
 }
 
