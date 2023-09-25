54e91d829962bb87fed55bee84dd8b09369ec5e2
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-25 21:45:20 +0800

    Update index.php

--- CloudComputing/index.php
+++ CloudComputing/index.php
@@ -7,6 +7,7 @@
 		<title>TARUMT</title>
 		<meta charset = "UTF-8" />
 		<meta name = "viewport" content = "width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel = "stylesheet" type = "text/css" href = "http://serene-bucket.s3-website-us-east-1.amazonaws.com/css/bootstrap.css"" />
 		<link rel = "stylesheet" type = "text/css" href = "http://serene-bucket.s3-website-us-east-1.amazonaws.com/css/style.css" />
 		<link rel = "stylesheet" type = "text/css" href = "http://serene-bucket.s3-website-us-east-1.amazonaws.com/css/jquery-ui.css" />
