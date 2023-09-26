4de18327ba490c158fa5b2b0a807a26213cc311f
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-26 23:44:58 +0800

    Add files via upload

--- CloudComputing/index.php
+++ CloudComputing/index.php
@@ -1,55 +1,51 @@
 <!DOCTYPE html>
-<?php
-	require 'connect.php';
-?>
-<html lang = "en">
+<html lang = "eng">
 	<head>
-		<title>TARUMT</title>
+		<title>Internship portal management system</title>
 		<meta charset = "UTF-8" />
 		<meta name = "viewport" content = "width=device-width, initial-scale=1" />
-		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/bootstrap.css"" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/bootstrap.css" />
 		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/style.css" />
 		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/jquery-ui.css" />
-		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/jquery.dataTables.css" />
 	</head>
-	
 <body>
 <!--------------------HEAD---------------------->
 <?php include'head.php'?>
 <!--------------------HEAD---------------------->
-<br />
-<br />
-<br />
-<?php
-	$date = date("Y-m-d", strtotime("+8 HOURS"));
-	$q_activity = $conn->query("SELECT * FROM `activity` WHERE '$date' BETWEEN `start` AND `end`") or die($conn->error);
-	$f_activity = $q_activity->fetch_array();
-	$v_activity = $q_activity->num_rows;
-	if($v_activity > 0){	
-		echo '<marquee><h4 class = "text-danger">'.$f_activity['title'].' - '.$f_activity['description'].'</h4></marquee>';
-	}
-?>
-	
-	</div>
-	<div class = "container-fluid" id = "content">	
-		<div class = "row" style = "margin-top:-200px;">	
-			
-			
-				<ul class="nav nav-tabs">
-					<li class="active"><a href="#home" data-toggle = "tab">Home</a></li>
-					<li><a href="#aboutus" data-toggle = "tab">About us</a></li>
-					<li><a href="#activities" data-toggle = "tab">Internships</a></li>
-				</ul>
-				<br />
-				<div class = "tab-content container-fluid">
-					<?php
-						include 'home.php';
-						include	'aboutus.php';
-						include	'activities.php';
-					?>
-				
+	<div class = "container-fluid" id = "content">
+		<div class = "row">	
+			<div class = "col-md-2">	
 			</div>
-		</div>
+			<div class = "col-md-2">	
+			</div>
+			<div class = "col-md-4">	
+				<div class = "panel panel-primary">
+					<div class = "panel-heading">
+						<h4>Administrator Login</h4>
+					</div>
+					<div class = "panel-body">
+						<form method = "POST" enctype = "multipart/form-data">
+							<div class = "form-group">
+								<label>Username</label>
+								<input type = "text" id = "username" class = "form-control" />
+							</div>
+							<div class = "form-group">
+								<label>Password</label>
+								<input type = "password" maxlength = "12" id = "password" class = "form-control" />
+							</div>
+							<br />
+							<div id = "loading">
+							</div>
+							<br />
+							<div class = "form-group">
+								<button type = "button" class = "btn btn-primary form-control" id = "login"><span class = "glyphicon glyphicon-log-in"></span> Login</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>	
 	</div>
 	<br />
 	<br />
@@ -60,13 +56,11 @@
 	<br />
 	<br />
 	<br />
-	<nav class = "navbar-default" id = "footer" style="background-color: #0080d2;">
-		<label class = "navbar-brand pull-right">&copy;Internship portal management system<?php echo date('Y', strtotime('+8 HOURS'))?></label>
-		<label class = "navbar-brand ">penang@tarc.edu.my</label>
+	<nav class = "navbar-default" id = "footer">
+		<label class = "navbar-brand pull-right">&copy; Internship portal management system <?php echo date('Y', strtotime('+8 HOURS'))?></label>
+		<label class = "navbar-brand ">TREMENDOUSCHATIKOBO@GMAIL.COM</label>
 	</nav>
 </body>	
-<script src = "js/jquery-3.1.1.js"></script>
-<script src = "js/script.js"></script>
-<script src = "js/bootstrap.js"></script>
-<script src = "js/jquery.dataTables.min.js"></script>
+<script src = "../js/jquery-3.1.1.js"></script>
+<script src = "../js/script.js"></script>
 </html>
--- CloudComputing/transaction.php
+++ CloudComputing/transaction.php
@@ -4,7 +4,7 @@
 ?>
 <html lang = "eng">
 	<head>
-		<title>InternMSU</title>
+		<title>Internship portal management system</title>
 		<meta charset = "UTF-8" />
 		<meta name = "viewport" content = "width=device-width, initial-scale=1" />
 		<link rel = "stylesheet" type = "text/css" href = "http://sereneooi-bucket.s3-website-us-east-1.amazonaws.com/css/bootstrap.css" />
@@ -20,21 +20,21 @@
 <!-------------------SIDEBAR------------------>
 
 		<div id = "sidecontent" class = "well pull-right">
-			<div class = "alert alert-info">Home</div>
-			<div class = "alert alert-success"><center><h3>VISION</h3></center></div>
-			<h4 style = "text-indent:50px;">Excellence in producing highly skilled, well qualified students in the job industry.</h4>
-			<br />
-			<div class = "alert alert-success"><center><h3>MISSION</h3></center></div>
-			<h4 style = "text-indent:50px;">Committed to provide affordable and high quality workers.</h4>
-			<br />
-			<div class = "alert alert-success"><center><h3>OBJECTIVE</h3></center></div>
-			<ol>
-				<li><h4>Possess knowledge and skills that would prepare students to improve their working skills through work related learning </h4></li>
-				<li><h4>Prepare student to be well equiped in the job industry . </h4></li>
-				<li><h4>Be proficient in designing and developing computing solutions</h4></li>
-
-			</ol>
-		<br /><br /><br />
+			<div class = "alert alert-info">Transaction</div>
+			<div class = "panel panel-default">
+				<div class = "panel-heading">
+					<div class = "form-inline">
+						<label>Enter Student ID:</label>
+						<input type = "number" style = "width:200px;" class = "form-control" min = "0" max = "999999" id = "search"/>
+						<button class = "btn btn-primary" id = "btn_search"><span class = "glyphicon glyphicon-search"></span></button>
+					</div>
+					<hr style = "border-top:1px dotted #000;"/>
+					<div id = "result">
+						
+					</div>
+					<br style = "clear:both;"/>
+				</div>
+			</div>
 		</div>
 	<br />
 	<br />
@@ -46,10 +46,13 @@
 	<br />
 	<br />
 	<nav class = "navbar-default" id = "footer">
-		<label class = "navbar-brand pull-right">&copy; Internship portal management system  <?php echo date('Y', strtotime('+8 HOURS'))?></label>
-		<label class = "navbar-brand ">tremendouschatikobo@gmail.com</label>
+		<label class = "navbar-brand pull-right">&copy; Internship portal management system <?php echo date('Y', strtotime('+8 HOURS'))?></label>
+		<label class = "navbar-brand ">chatikobo</label>
 	</nav>
 </body>	
 <script src = "../js/jquery-3.1.1.js"></script>
 <script src = "../js/sidebar.js"></script>
+<script src = "../js/bootstrap.js"></script>
+<script src = "../js/sidebar.js"></script>
+<script src = "../js/script.js"></script>
 </html>
