14f7e0912be7d17d4156447781205e2f2f447780
sereneooi <86998336+sereneooi@users.noreply.github.com>
2023-09-26 22:43:15 +0800

    Add files via upload

--- CloudComputing/file_uploading/dbconfig.php
+++ CloudComputing/file_uploading/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "db1.cpg9cjdwwtu2.us-east-1.rds.amazonaws.com";
+$dbhost = "db.cx03wpfilkwh.us-east-1.rds.amazonaws.com";
 $dbuser = "main";
 $dbpass = "qwer1234";
-$dbname = "db1";
+$dbname = "db";
 $conn= mysqli_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
 mysqli_select_db($conn,$dbname) or die('database selection problem');
 ?>
